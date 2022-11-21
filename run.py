95e11c9439cfeddf716a7f6278389e2de81a2f08
Corentin-Vidick <corentin.vidick@gmail.com>
2022-11-21 15:27:13 +0000

    Create global map array in create_map function

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
-print("Welcome to Coco's Dungeon Crawler")
+def create_map():
+    """
+    Creates map array
+    Initialise player position
+    Initialise exit position
+    """
+    print("Creating map")
+    global map
+    rows, cols = (5, 5)
+    map = [["empty" for i in range(cols)] for j in range(rows)]
+    map[0][0] = "player"
+    map[4][4] = "exit"
+    print(map)
+
+
+def main():
+    """
+    Main function, runs the game
+    """
+    print("Start game")
+    create_map()
+
+
+main()
