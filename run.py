727409f59f7ab8ca77eebe1c292bdf50bc833125
Corentin-Vidick <corentin.vidick@gmail.com>
2022-11-22 17:23:31 +0000

    Debug and style UI

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import random
 
 # Global variables
 player = {
-    "room": 1,
+    "room": 0,
     "life": 2
 }
 
@@ -32,20 +32,14 @@ rooms = []
 class Room:
     """
     Creates an empty, dark room
+    status matches legend options
+    sontents to add future objects (sword, shield...)
+    pos
     """
-    def __init__(self, status, contents, row, column):
+    def __init__(self, status, contents, pos):
         self.status = status
         self.contents = contents
-        self.row = row
-        self.column = column
-
-
-def print_positions():
-    """
-    Temporary function to print player's position
-    """
-    print(f"Player:{player}")
-    print(f"Skeleton:{skel}")
+        self.pos = pos
 
 
 def create_map():
@@ -55,12 +49,11 @@ def create_map():
     Initialise exit position
     """
     # Create empty map
-    for x in range(5):
-        for y in range(5):
-            room = Room("empty", "nothing", x, y)
-            rooms.append(room)
+    for i in range(25):
+        room = Room("empty", "nothing", i)
+        rooms.append(room)
     # Place player
-    rooms[1].status = "player"
+    rooms[0].status = "player"
     # Place exit door
     rooms[24].status = "door"
     # Place skeleton
@@ -73,7 +66,7 @@ def player_action_choice():
     Rest, see map or move
     """
     choice = input("Would you like to rest ('1'), look at your map ('2')\
- or move('3')?")
+ or move('3')?\n...")
     if choice == "1":
         rest()
     elif choice == "2":
@@ -102,13 +95,13 @@ def show_map():
     """
     Shows the map to the player and how many lives they have left
     """
-    print("Map:\n")
+    print("\nMap:\n")
     # one line the assignement and print?
     map_design = draw_map()
     print(map_design)
     # end of one line?
     life = player["life"]
-    print(f"\nYou have {life} lives left")
+    print(f"You have {life} health left\n")
 
 
 def draw_map():
@@ -117,8 +110,8 @@ def draw_map():
     """
     map_string = ""
     pos = 0
-    for x in range(5):
-        for y in range(5):
+    for i in range(5):
+        for j in range(5):
             map_string += (legend[rooms[pos].status])
             pos += 1
         map_string += "\n"
@@ -130,28 +123,28 @@ def player_movement_choice():
     Asks for input to move player
     Only gives possible options
     """
-    print("Checking possible movements...\n")
-    move_up, move_down, move_left, move_right = (True, True, True, True)
-    for i in range(25):
-        if rooms[i].status == "player":
-            move_up = False if i in (0, 1, 2, 3, 4) else True
-            move_down = False if i in (20, 21, 22, 23, 24) else True
-            move_left = False if i in (0, 5, 10, 15, 20) else True
-            move_right = False if i in (4, 9, 14, 19, 24) else True
+    print("\n...Checking possible movements...\n")
+    for room in (rooms):
+        if room.status == "player":
+            move_up = False if room.pos in (0, 1, 2, 3, 4) else True
+            move_down = False if room.pos in (20, 21, 22, 23, 24) else True
+            move_left = False if room.pos in (0, 5, 10, 15, 20) else True
+            move_right = False if room.pos in (4, 9, 14, 19, 24) else True
     print("Would you like to go:")
     if move_up:
-        print('"up" to go up')
+        print("go up ('1')")
     if move_down:
-        print('"down" to go down')
+        print("go down ('2')")
     if move_left:
-        print('"left" to go left')
+        print("go left ('3')")
     if move_right:
-        print('"right" to go right')
-    direction = input("I want to go...   ")
-    if direction not in ("up", "left", "down", "right"):
-        print("Wrong input, please try again\n")
+        print("go right ('4')")
+    direction = input("...")
+    if direction not in ("1", "2", "3", "4"):
+        print("\nWrong input, please try again\n")
         player_movement_choice()
-    move_player(direction)
+    else:
+        move_player(direction)
 
 
 def move_player(direction):
@@ -160,35 +153,33 @@ def move_player(direction):
     Otherwise returns to asking direction
     """
     # Needs some work on conditions for movement
-    print("Moving player...\n")
-#    previous_room = player["room"]
-    if direction == "up" and player["room"] > 4:
+    print("\n...Moving player...\n")
+    if direction == "1" and player["room"] > 4:
         rooms[player["room"]].status = "empty"
         player["room"] -= 5
         rooms[player["room"]].status = "player"
-    if direction == "down" and player["room"] < 19:
+    elif direction == "2" and player["room"] < 19:
         rooms[player["room"]].status = "empty"
         player["room"] += 5
         rooms[player["room"]].status = "player"
-    if direction == "left" and player["room"] not in (0, 5, 10, 15, 20):
+    elif direction == "3" and player["room"] not in (0, 5, 10, 15, 20):
         rooms[player["room"]].status = "empty"
         player["room"] -= 1
         rooms[player["room"]].status = "player"
-    if direction == "right" and player["room"] not in (4, 9, 14, 19, 24):
+    elif direction == "4" and player["room"] not in (4, 9, 14, 19, 24):
         rooms[player["room"]].status = "empty"
         player["room"] += 1
         rooms[player["room"]].status = "player"
-#    if player["room"] < 0 or player["room"] > 24:
-#        player["room"] = previous_room
-#        print("Move impossible, please try again")
-#        player_movement_choice()
+    else:
+        print("Impossible move, please try again\n")
+        player_movement_choice()
 
 
 def check_movement_result():
     """
     Checks what the player has encountered after their move
     """
-    print("Checking where you are...\n")
+    print("\n...Checking where you are...\n")
     if player["room"] == door["room"]:
         victory()
     if player["room"] == skel["room"]:
@@ -200,18 +191,11 @@ def encounter():
     When player encounters a skeleton
     """
     fight_or_flight = input("You have encountered a skeleton! Would you like \
-to fight ('Y') or retreat ('N')?   ")
-    if fight_or_flight == "Y":
+to fight ('1') or retreat ('2')?\n...")
+    if fight_or_flight == "1":
         fight()
-    elif fight_or_flight == "N":
-        direction = random.randint(1, 2)
-        print(direction)
-        if direction == 1:
-            print("You failed to escape, you have to fight\n")
-            fight()
-        elif direction == 2:
-            print("You run away...\n")
-            player_movement_choice()
+    elif fight_or_flight == "2":
+        flight()
     else:
         print("Wrong input, please try again\n")
         encounter()
@@ -219,12 +203,25 @@ to fight ('Y') or retreat ('N')?   ")
 
 def fight():
     """
-    Fight against skeleton
+    Fight against a skeleton
     """
     print("You fight the skeleton. He hits you first \
 but you manage to defeat it!")
     player["life"] -= 1
     rooms[skel["room"]].status = "player"
+
+
+def flight():
+    """
+    Flee from a skeleton
+    """
+    direction = random.randint(1, 2)
+    if direction == 1:
+        print("You failed to escape, you have to fight\n")
+        fight()
+    elif direction == 2:
+        print("You run away...\n")
+        player_movement_choice()
 
 
 def victory():
@@ -233,7 +230,7 @@ def victory():
     Resets player position and life
     """
     print("\n\nYou made it out!\n!!!Congratulations!!!\n\n")
-    player["row"], player["column"] = (0, 0)
+    player["room"] = 0
     player["life"] = 2
     main()
 
@@ -244,7 +241,7 @@ def defeat():
     Resets player position
     """
     print("\n\nYou died!\n!!!Better luck next time!!!\n\n")
-    player["row"], player["column"] = (0, 0)
+    player["room"] = 0
     player["life"] = 2
     main()
 
@@ -257,7 +254,6 @@ def main():
     create_map()
     while player["life"]:
         player_action_choice()
-        print_positions()
     defeat()
 
 
